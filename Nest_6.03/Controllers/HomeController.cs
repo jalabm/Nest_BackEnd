caccb9cce96b94243abeca3257000fea1ea735bd
Jalə <jal@Jale-MacBook-Air.local>
2024-03-07 13:01:49 +0400

    .

---
+++ Nest_6.03/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nest_6._03.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
+
---
+++ Nest_6.03/Controllers/ShopLeftController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Nest_6._03.Controllers
+{
+    public class ShopLeftController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
